bbb6fc6bfa549b9080eb2eb98ca0b28368e443ce
occupty <80610662+occupty@users.noreply.github.com>
2023-05-15 21:33:06 +0800

    Update heatmap.R
    Use dplyr package to generate support data for heatmap

--- 相关性热图_R/heatmap.R
+++ 相关性热图_R/heatmap.R
@@ -1,8 +1,8 @@
 # 三个参数：fpkm数据，代谢物表达量数据，输出文件名
-# 该脚本直接在linux命令行格式下运行 Rscript heatmap.R fpkm.xls compounds.xls filename
 args <- commandArgs(TRUE)
 library("psych")
 library("pheatmap")
+library("tidyverse")
 fpkm <- read.table(args[1], header = T, row.names = 1, sep = "\t") # 导入fpkm数据，数据格式：gene_id\tsample1\tsample2\t ....
 compd <- read.table(args[2], header = T, row.names = 1, sep = "\t") # 导入代谢物数据，数据格式：compound\tsample1\tsample2\t ....
 fpkm.t <- t(fpkm) # 转置用于corr.test函数，corr.test()识别每列为一条信息
@@ -15,4 +15,10 @@ pheatmap(cortest_psy$r,number_color = "black", # 在每个cell中填写内容的
 	display_numbers = matrix(ifelse(cortest_psy$p<0.010,"**",ifelse(cortest_psy$p>0.010 & cortest_psy$p<0.050,"*","")), nrow(cortest_psy$p)), # 在每个cell中填写内容的设置，0.01<p<0.05为"*", p<0.01为"**"
 	scale = "row", # 按行进行标准化，可选参数'none', 'row' or 'column'
 	border_color = NA, # 去除cell分割线
-	filename = args[3]) # 设置文件名
+	filename = paste0(args[3], ".pdf")) # 设置文件名
+psy_r <- data.frame(Compound = rownames(cortest_psy$r), cortest_psy$r)
+psy_p <- data.frame(Compound = rownames(cortest_psy$p), cortest_psy$p)
+psy_r <- gather(psy_r, key = "Gene", value = "R", -Compound);
+psy_p <- gather(psy_p, key = "Gene", value = "Pvalue", -Compound);
+psy_out <- merge(psy_r, psy_p, by = c("Compound", "Gene"))
+write.table(psy_out,paste0(args[3],"_R_P.xls"), sep="\t", quote = F, row.names = F)
